7ed220098e6d2818c3d56b20422a9cb4f39fc7d4
RomanticCoder <67548512+RomanticCoder@users.noreply.github.com>
2020-09-30 03:42:18 +0900

    A2 - test

--- assignment2/index.php
+++ assignment2/index.php
@@ -1,54 +1,57 @@
 <?php
 
-    //set default value of variables for initial page load
-    if (!isset($investment)) { $investment = ''; } 
-    // this is where you should check to see if the interest_rate and $years are set
-    if (!isset($interest_rate)) { $interest_rate = ''; } 
-    if (!isset($years)) {$years = '';}
+//set default value of variables for initial page load
+if (!isset($investment)) {
+  $investment = '';
+}
+// this is where you should check to see if the interest_rate and $years are set
+if (!isset($interest_rate)) {
+  $interest_rate = '';
+}
+if (!isset($years)) {
+  $years = '';
+}
 
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>Future Value Calculator</title>
-    <link rel="stylesheet" type="text/css" href="main.css">
+  <title>Future Value Calculator</title>
+  <link rel="stylesheet" type="text/css" href="main.css" />
 </head>
 
 <body>
-    <main>
+  <main>
     <h1>Future Value Calculator</h1>
     <?php
     //This code checks to see if we got an error message from the display_result.php page
-     if (!empty($error_message)) { ?>
-        <p class="error"><?php echo htmlspecialchars($error_message); ?></p>
+    if (!empty($error_message)) { ?>
+      <p class="error"><?php echo htmlspecialchars($error_message); ?></p>
     <?php } ?>
     <form action="display_results.php" method="post">
+      <div id="data">
+        <label>Investment Amount:</label>
+        <input type="text" name="investment" value="<?php echo htmlspecialchars($investment); ?>" />
+        <br />
 
-        <div id="data">
-            <label>Investment Amount:</label>
-            <input type="text" name="investment"
-                   value="<?php echo htmlspecialchars($investment); ?>">
-            <br>
+        <!-- This is where you should put the Interest Rate -->
+        <label>Yearly Interest Rate:</label>
+        <input type="text" name="interest_rate" value="<?php echo htmlspecialchars($interest_rate); ?>" />
+        <br />
 
-            <!-- This is where you should put the Interest Rate -->
-            <label>Yearly Interest Rate:</label>
-            <input type="text" name="interest_rate"
-                   value="<?php echo htmlspecialchars($interest_rate); ?>">
-            <br>
+        <!-- This is where you should put the Years input -->
+        <label>Number of Years:</label>
+        <input type="text" name="years" value="<?php echo htmlspecialchars($years); ?>" />
+        <br />
+      </div>
 
-            <!-- This is where you should put the Years input -->
-            <label>Number of Years:</label>
-            <input type="text" name="years"
-                   value="<?php echo htmlspecialchars($years); ?>">
-            <br>
-        </div>
-
-        <div id="buttons">
-            <label>&nbsp;</label>
-            <input type="submit" value="Calculate"><br>
-        </div>
-
+      <div id="buttons">
+        <label>&nbsp;</label>
+        <input type="submit" value="Calculate" /><br />
+      </div>
     </form>
-    </main>
+  </main>
 </body>
+
 </html>
